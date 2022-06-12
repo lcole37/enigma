71498220305c1bc12f70a052dc6b8b9fddd5ba32
Lucas Colwell <colwellc37@gmail.com>
2022-06-12 16:24:46 -0600

    Feat: create offsets method

--- lib/generator.rb
+++ lib/generator.rb
@@ -4,13 +4,25 @@ class Generator
   end
 
   def keys(key)
-    key_array = key.split("")
     keys_hash = {}
     #use index to enumerate instead of hard code? letters?
-    keys_hash[:A] = (key_array[0] + key_array[1]).to_i
-    keys_hash[:B] = (key_array[1] + key_array[2]).to_i
-    keys_hash[:C] = (key_array[2] + key_array[3]).to_i
-    keys_hash[:D] = (key_array[3] + key_array[4]).to_i
+    keys_hash[:A] = (key[0] + key[1]).to_i
+    keys_hash[:B] = (key[1] + key[2]).to_i
+    keys_hash[:C] = (key[2] + key[3]).to_i
+    keys_hash[:D] = (key[3] + key[4]).to_i
     return keys_hash
+  end
+
+  def offsets(date_string)
+    date_squared = date_string.to_i ** 2
+    last_4_string = date_squared.to_s[-4..-1]
+    offsets_hash = {}
+    # does this need to be in a hash?
+    #use index to enumerate instead of hard code?
+    offsets_hash[:A] = (last_4_string[0].to_i)
+    offsets_hash[:B] = (last_4_string[1].to_i)
+    offsets_hash[:C] = (last_4_string[2].to_i)
+    offsets_hash[:D] = (last_4_string[3].to_i)
+    return offsets_hash
   end
 end
