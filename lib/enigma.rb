edbaf1053fff21e41419bf93bd63e51648ac0548
Lucas Colwell <colwellc37@gmail.com>
2022-06-12 13:11:01 -0600

    Feat: create key generator with random method

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,5 +1,14 @@
+require 'date'
 class Enigma
-  # def initialize(argument)
-  #    @argument = argument
-  # end
+  def initialize
+     @date_string = Date.today.strftime("%d%m%y")
+     @alphabet = ("a".."z").to_a << " "
+  end
+
+  def encrypt(message, key, date)
+    encryption_array = []
+    message_array = message.split
+    message_array.each_with_index(0) do |letter|
+      ecryption_array << alphabet(letter).shift(A)
+  end
 end
