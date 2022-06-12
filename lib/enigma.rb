8507ff15f43c618ea56eec48411a5eee9b820acd
Lucas Colwell <colwellc37@gmail.com>
2022-06-12 16:42:33 -0600

    Feat: sudocode

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -3,17 +3,22 @@ class Enigma
   def initialize
      @date_string = Date.today.strftime("%d%m%y")
      @alphabet = ("a".."z").to_a << " "
+     @generator = Generator.new
   end
 
   def encrypt(message, key, date)
     encryption_array = []
-    shifts = Shifts.final_shifts(key)
-    message_array = message.split
+    message_array = message.split("")
+    keys = generator.keys(key)
+    offsets = generator.offsets(date)
+    shifts = generator.shifts(keys, offsets)
 
-    shifts.each do |shift|
-      message_array.each_with_index(0) do |letter|
-        ecryption_array << ____ + shift
-      end
+    shifts.values.each do |shift|
+      # split into 4 arrays, or x arrays of 4 a-d
+      # enumerate over those, shifting by corresponding a-d
+      # message_array.each_with_index(0) do |letter|
+      #   ecryption_array << ____ + shift
+      # end
     end
   end
 end
