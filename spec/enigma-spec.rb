e2cebc98fa80b3f902d5d99a4e068a328c6ee879
Lucas Colwell <colwellc37@gmail.com>
2022-06-12 11:43:07 -0600

    Initial commit

---
+++ spec/enigma-spec.rb
@@ -0,0 +1,19 @@
+require "rspec"
+require "./lib/enigma"
+
+RSpec.describe Enigma do 
+	describe "Iteration 1" do
+		before :each do
+			@
+			@
+		end
+
+		it "exists" do
+			expect(@enigma).to be_a(Class)
+		end
+
+		it "has attributes" do
+			expect(@enigma.attribute).to eq()
+    end
+	end
+end
