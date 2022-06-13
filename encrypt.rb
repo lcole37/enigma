0f8207e8d169341f7967be6e96a5a200f1b31fd4
Lucas Colwell <colwellc37@gmail.com>
2022-06-12 23:27:10 -0600

    Feat: sudocode

---
+++ encrypt.rb
@@ -0,0 +1,18 @@
+require './lib/key_generator'
+require './lib/generator'
+require './lib/enigma'
+
+# def initialize(argument) use argv here
+#   @argument = argument
+# end
+#file1.read
+#variable = message
+#file1.close
+#file2.write
+# => enigma = Enigma.new
+# => encryption = enigma.encrypt(message)
+# put message
+#file2.close
+
+
+puts "Created #{'encrypted.txt'} with the key #{encryption[:key]} and date #{encryption[:date]}"
