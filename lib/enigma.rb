7736c7c25be7bdd1ad996aadd583d1dfe9bdeb7b
Lucas Colwell <colwellc37@gmail.com>
2022-06-12 22:08:47 -0600

    Feat: create decrypt method

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -27,13 +27,28 @@ class Enigma
         encryption_array << post_shift_letter
       end
     end
-      # require "pry"; binding.pry
     encrypted_message = encryption_array.join
-
     encypted_hash = {encryption: encrypted_message, key: key, date: date}
   end
 
-  def decrypt(message, key, date = date_string)
+  def decrypt(message, key = @keygenerator.random_key, date = @date_string)
+    decryption_array = []
+    message_array = message.downcase.chars
+    generator = Generator.new(message, key, date)
+    shifts_array = generator.final_shifts.map{|shift| shift[1]}
+
+    message_array.each_with_index do |letter, index|
+      if @alphabet.index(letter).nil?
+        letter
+      else
+        pre_shift_alphabet = @alphabet.rotate(shifts_array[index % 4])
+        pre_shift_index = pre_shift_alphabet.index(letter)
+        post_shift_letter = @alphabet[pre_shift_index]
+        decryption_array << post_shift_letter
+      end
+    end
+    decrypted_message = decryption_array.join
+    decrypted_hash = {decryption: decrypted_message, key: key, date: date}
 
   end
 end
