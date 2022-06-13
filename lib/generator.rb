3d3e59c14e315e540cdf6ef250bf44413bda670d
Lucas Colwell <colwellc37@gmail.com>
2022-06-12 18:45:45 -0600

    Feat: refactor to not rely on args

--- lib/generator.rb
+++ lib/generator.rb
@@ -1,32 +1,51 @@
 class Generator
+
+  def initialize(name, key, date)
+    @name = name
+    @key_string = key
+    @date_string = date
+    @keys_hash = {}
+    @offsets_hash = {}
+  end
   def random_key
     number = "0" + 4.times.map{rand(10)}.join
   end
 # => these should be in an encryptor class, not generator
-  def keys(key)
-    keys_hash = {}
-    #use index to enumerate instead of hard code? letters?
-    keys_hash[:A] = (key[0] + key[1]).to_i
-    keys_hash[:B] = (key[1] + key[2]).to_i
-    keys_hash[:C] = (key[2] + key[3]).to_i
-    keys_hash[:D] = (key[3] + key[4]).to_i
-    return keys_hash
+  def keys
+    key_array = @key_string.split("")
+    @keys_hash[:A] = (@key_string[0] + @key_string[1]).to_i
+    @keys_hash[:B] = (@key_string[1] + @key_string[2]).to_i
+    @keys_hash[:C] = (@key_string[2] + @key_string[3]).to_i
+    @keys_hash[:D] = (@key_string[3] + @key_string[4]).to_i
+    return @keys_hash
   end
 
-  def offsets(date_string)
-    date_squared = date_string.to_i ** 2
+  # def keys(key) using enumerable. longer lines
+  #   key_array = key.split("")
+  #   keys_array = []
+  #   index = 0
+  #   key_array.each do |number|
+  #     # require "pry"; binding.pry
+  #     keys_array << (number + key_array[index + 1])
+  #     index += 1
+  #     break if keys_array.length == 4
+  #   end
+  #   return keys_array
+  # end
+
+  def offsets
+    date_squared = @date_string.to_i ** 2
     last_4_string = date_squared.to_s[-4..-1]
-    offsets_hash = {}
-    # does this need to be in a hash?
-    #use index to enumerate instead of hard code?
-    offsets_hash[:A] = (last_4_string[0].to_i)
-    offsets_hash[:B] = (last_4_string[1].to_i)
-    offsets_hash[:C] = (last_4_string[2].to_i)
-    offsets_hash[:D] = (last_4_string[3].to_i)
-    return offsets_hash
+    @offsets_hash[:A] = (last_4_string[0].to_i)
+    @offsets_hash[:B] = (last_4_string[1].to_i)
+    @offsets_hash[:C] = (last_4_string[2].to_i)
+    @offsets_hash[:D] = (last_4_string[3].to_i)
+    return @offsets_hash
   end
 
-  def final_shifts(keys, offsets)
-    keys.merge(offsets){ |k, key, offset| key + offset }
+  def final_shifts
+    keys
+    offsets
+    @keys_hash.merge(@offsets_hash){ |k, key, offset| key + offset }
   end
 end
