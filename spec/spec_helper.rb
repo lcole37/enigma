775c68f89fc6588dfef5ca4ef44d85eef357f9d7
Lucas Colwell <colwellc37@gmail.com>
2022-06-13 14:25:03 -0600

    Test: add require to spec helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,5 @@
 require 'simplecov'
 require_relative "enigma_spec"
 require_relative "generator_spec"
+require_relative "key_generator_spec"
 SimpleCov.start
-
-# require_relative "_spec"
-# require_relative "_spec"
