bc098f0d66d905efcc8c97fd4f5c0efaf4f5e764
Lucas Colwell <colwellc37@gmail.com>
2022-06-13 00:28:57 -0600

    Feat: create decrypt runner

--- decrypt.rb
+++ decrypt.rb
@@ -1,18 +1,11 @@
-require './lib/key_generator'
-require './lib/generator'
 require './lib/enigma'
 
-# def initialize(argument) use argv here
-#   @argument = argument
-# end
-#file1.read
-#variable = message
-#file1.close
-#file2.write
-# => enigma = Enigma.new
-# => decryption = enigma.decrypt(message, key)
-# write message
-#file2.close
+@enigma = Enigma.new
+file1 = File.open(ARGV[0], "r")
+message = file1.read
+file2 = File.open(ARGV[1], "w")
+decryption = enigma.decrypt(message)
+file2.write(decryption)
+file2.close
 
-
-puts "Created #{created_file} with the key #{decryption[:key]} and date #{decryption[:date]}"
+puts "Created #{ARGV[1]} with the key #{decryption[:key]} and date #{decryption[:date]}"
