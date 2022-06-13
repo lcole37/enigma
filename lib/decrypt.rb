7dde8b685b3a32d3153fc7183e65f534c9960e20
Lucas Colwell <colwellc37@gmail.com>
2022-06-13 14:22:20 -0600

    Refactor: delete extra lines

--- lib/decrypt.rb
+++ lib/decrypt.rb
@@ -1,12 +1,10 @@
 require './lib/enigma'
 
 enigma = Enigma.new
-
 file1 = File.open(ARGV[0], "r")
 message = file1.read
 file2 = File.open(ARGV[1], "w")
 decryption = enigma.decrypt(message, ARGV[2], ARGV[3])
 file2.write(decryption)
 file2.close
-
 puts "Created #{ARGV[1]} with the key #{ARGV[2]} and date #{ARGV[3]}"
