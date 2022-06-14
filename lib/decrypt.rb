4b93d8b2f506e44344c77a3dd564b6ec463b9492
Lucas Colwell <colwellc37@gmail.com>
2022-06-14 17:16:31 -0600

    Fix decrypt runner, must include argument 3 and 4

--- lib/decrypt.rb
+++ lib/decrypt.rb
@@ -1,20 +1,10 @@
 require './lib/enigma'
 
-if ARGV[0] == nil  #added in case user does not enter file input
-  file_to_read = "encrypted.txt"
-else
-  file_to_read = ARGV[0]
-end
-if ARGV[1] == nil #added in case user does not enter file output
-  file_to_write = "decrypted.txt"
-else
-  file_to_write = ARGV[1]
-end
 enigma = Enigma.new
-file1 = File.open(file_to_read, "r")
+file1 = File.open(ARGV[0], "r")
 message = file1.read
-file2 = File.open(file_to_write, "w")
+file2 = File.open(ARGV[1], "w")
 decryption = enigma.decrypt(message, ARGV[2], ARGV[3])
 file2.write(decryption)
 file2.close
-puts "Created #{file_to_write} with the key #{ARGV[2]} and date #{ARGV[3]}"
+puts "Created #{ARGV[1]} with the key #{ARGV[2]} and date #{ARGV[3]}"
