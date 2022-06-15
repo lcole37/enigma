5ef0e830e3710668a1d2e584cf2e2c610b8e6d5a
Lucas Colwell <colwellc37@gmail.com>
2022-06-15 12:45:42 -0600

    Feat: add shift_text helper method for encrypt and decrypt

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -3,49 +3,49 @@ require_relative 'generator'
 require_relative 'key_generator'
 
 class Enigma
+  attr_reader :alphabet,
+              :date_string
+
   def initialize
      @date_string = Date.today.strftime("%d%m%y")
      @alphabet = ("a".."z").to_a << " "
      @keygenerator = KeyGenerator.new
   end
 
-  def encrypt(message, key = @keygenerator.random_key, date = @date_string)
-    alphabet = @alphabet
-    encryption_chars = []
+  def encrypt(message, key = @keygenerator.random_key, date = date_string)
     message_array = message.downcase.chars
     generator = Generator.new(message, key, date)
     shifts_array = generator.final_shifts.map{|shift| shift[1]}
-    message_array.each_with_index do |letter, index|
-      if alphabet.index(letter).nil?
-        encryption_chars << letter
-      else
-        pre_shift_index = alphabet.index(letter)
-        post_shift_alphabet = alphabet.rotate(shifts_array[index % 4])
-        post_shift_letter = post_shift_alphabet[pre_shift_index]
-        encryption_chars << post_shift_letter
-      end
-    end
-    encrypted_message = encryption_chars.join
+    encrypted_message = shift_text(message_array, shifts_array, "encrypt")
     encypted_hash = {encryption: encrypted_message, key: key, date: date}
   end
 
-  def decrypt(message, key = @keygenerator.random_key, date = @date_string)
-    alphabet = @alphabet
-    decryption_chars = []
+  def decrypt(message, key = @keygenerator.random_key, date = date_string)
     message_array = message.downcase.chars
     generator = Generator.new(message, key, date)
     shifts_array = generator.final_shifts.map{|shift| shift[1]}
+    decrypted_message = shift_text(message_array, shifts_array, "decrypt")
+    decrypted_hash = {decryption: decrypted_message, key: key, date: date}
+  end
+
+  def shift_text(message_array, shifts_array, crypt_type)
+    shifted_text_chars = []
     message_array.each_with_index do |letter, index|
       if alphabet.index(letter).nil?
-        decryption_chars << letter
+        shifted_text_chars << letter
       else
-        pre_shift_alphabet = alphabet.rotate(shifts_array[index % 4])
-        pre_shift_index = pre_shift_alphabet.index(letter)
-        post_shift_letter = alphabet[pre_shift_index]
-        decryption_chars << post_shift_letter
+        if crypt_type == "encrypt"
+          pre_shift_index = alphabet.index(letter)
+          post_shift_alphabet = alphabet.rotate(shifts_array[index % 4])
+          post_shift_letter = post_shift_alphabet[pre_shift_index]
+        else
+          pre_shift_alphabet = alphabet.rotate(shifts_array[index % 4])
+          pre_shift_index = pre_shift_alphabet.index(letter)
+          post_shift_letter = alphabet[pre_shift_index]
+        end
+        shifted_text_chars << post_shift_letter
       end
     end
-    decrypted_message = decryption_chars.join
-    decrypted_hash = {decryption: decrypted_message, key: key, date: date}
+    shifted_text_chars.join
   end
 end
